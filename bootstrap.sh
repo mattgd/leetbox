4fffae22553a601e56a742d27e602651caa84627
mattgd <mattdzwonczyk@gmail.com>
2019-03-09 10:13:57 -0500

    Add python2 installation.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y make
 sudo apt-get install -y gdb
 sudo apt-get install -y radare2
 sudo apt-get install -y python3-pip
+sudo apt-get install -y python2.7 python-pip
 
 # ROP
 sudo pip3 install capstone
